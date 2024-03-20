71b54184eb66e46a97cf2a3cd29bb3586c2ce720
Daniel Stocks <daniel@webcloud.se>
2024-03-20 11:57:01 +0100

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,109 +1,49 @@
-// # TODO: Refactor/Cleanup v0.1
-
+// # TODO: Unit tests
 import "./style.css";
+import { renderGrid, renderUpcomingPipes } from "./render";
+import { AVAILABLE_PIPES, GRID_COLS, GRID_ROWS } from "./config";
+import { getRandomItemFromArray, getRandomIntegerBetween } from "./util";
 
-import { bend, straight, short, cross } from "./pipes";
-
-const GRID_COLS = 8;
-const GRID_ROWS = 5;
-
-function getRandomItemFromArray(array: string[]) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
-function getRandomIntegerBetween(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const availablePipes = ["h", "v", "ne", "nw", "se", "sw", "c"];
-
+// Initialize game state
 const upcomingPipes = [...Array(6).keys()].map(() => {
-  return getRandomItemFromArray(availablePipes);
+  return getRandomItemFromArray(AVAILABLE_PIPES);
 });
-
-const startingDirection = getRandomItemFromArray(["s", "w", "n", "e"]);
-const endingDirection = getRandomItemFromArray(["s", "w", "n", "e"]);
-
-const angles: {
-  [key: string]: number;
-} = {
-  s: 0,
-  w: 90,
-  n: 180,
-  e: 270,
-};
 
 let startRow = getRandomIntegerBetween(2, GRID_ROWS - 2);
 let startCol = getRandomIntegerBetween(2, GRID_COLS - 2);
-const start: [string, string] = [`${startRow},${startCol}`, "start"];
 
-// # TODO: implement end tile
-// 1. Pick random position (but not on edges, on or adjacent to starting tile)
-// if start row = 2, col=3
-// Forbidden tiles: 1:234 ,2:234, 3:234
-let endRow = 0;
-let endCol = 0;
+let endRow = startRow;
+let endCol = startCol;
 
-//while([ startRow  endRow )
-const end: [string, string] = [`${endRow},${endCol}`, "end"];
+let invalidEndPositions = [
+  `${startRow},${startCol}`,
+  `${startRow},${startCol + 1}`,
+  `${startRow},${startCol - 1}`,
+  `${startRow - 1},${startCol}`,
+  `${startRow - 1},${startCol + 1}`,
+  `${startRow - 1},${startCol - 1}`,
+  `${startRow + 1},${startCol}`,
+  `${startRow + 1},${startCol + 1}`,
+  `${startRow + 1},${startCol - 1}`,
+];
 
-// # TODO: prevent start & end pipes from being replaced
-const grid = new Map([start, end]);
-
-function renderUpcomingPipes() {
-  return upcomingPipes
-    .map((pipe) => {
-      return `<div>
-    ${(pipe == "se" && bend) || ""}
-    ${(pipe == "ne" && `<div class="rotate-270">${bend}</div>`) || ""}  
-    ${(pipe == "nw" && `<div class="rotate-180">${bend}</div>`) || ""}
-    ${(pipe == "sw" && `<div class="rotate-90">${bend}</div>`) || ""}
-    ${(pipe == "v" && straight) || ""}
-    ${(pipe == "h" && `<div class="rotate-90">${straight}</div>`) || ""}
-    ${(pipe == "c" && cross) || ""}
-    </div>`;
-    })
-    .join("");
+while (invalidEndPositions.includes(`${endRow},${endCol}`)) {
+  endRow = getRandomIntegerBetween(2, GRID_ROWS - 2);
+  endCol = getRandomIntegerBetween(2, GRID_COLS - 2);
 }
 
-function renderGrid() {
-  let gridOutput = "";
+const grid = new Map();
 
-  for (let i = 0; i < GRID_ROWS * GRID_COLS; i++) {
-    let row = Math.floor(i / GRID_COLS);
-    let col = i % GRID_COLS;
-    let pipe = grid.get(`${row},${col}`) || "";
+const state = {
+  grid,
+  upcomingPipes,
+  startRow,
+  startCol,
+  endRow,
+  endCol,
+};
 
-    gridOutput += `<div class="tile" data-row="${row}" data-col="${col}">
-    ${(pipe == "se" && bend) || ""}
-    ${(pipe == "ne" && `<div class="rotate-270">${bend}</div>`) || ""}  
-    ${(pipe == "nw" && `<div class="rotate-180">${bend}</div>`) || ""}
-    ${(pipe == "sw" && `<div class="rotate-90">${bend}</div>`) || ""}
-    ${(pipe == "v" && straight) || ""}
-    ${(pipe == "h" && `<div class="rotate-90">${straight}</div>`) || ""}
-    ${(pipe == "c" && cross) || ""}
-
-    ${
-      (pipe == "start" &&
-        `<div class="label">S</div>
-        <div class="rotate-${angles[startingDirection]}">${short}</div>`) ||
-      ""
-    }
-
-    ${
-      (pipe == "end" &&
-        `<div class="label">E</div>
-        <div class="rotate-${angles[endingDirection]}">${short}</div>`) ||
-      ""
-    }
-
-  </div>
-  `;
-  }
-
-  return gridOutput;
-}
-
+// Initial Game Render
 const gridEl = document.querySelector<HTMLDivElement>("#grid");
 const upcomingEl = document.querySelector<HTMLDivElement>("#upcoming-pipes");
 
@@ -114,31 +54,24 @@ if (upcomingEl === null) {
   throw new Error("Cannot find <div id='upcoming-pipes'>");
 }
 
-gridEl.innerHTML = renderGrid();
-upcomingEl.innerHTML = renderUpcomingPipes();
+gridEl.innerHTML = renderGrid(state);
+upcomingEl.innerHTML = renderUpcomingPipes(state);
 
 gridEl.addEventListener("click", function (event) {
   const target = event.target;
-
   if (target instanceof HTMLElement || target instanceof SVGElement) {
     const tile = target.closest(".tile");
-
-    let nextPipe = upcomingPipes.pop();
-    upcomingPipes.unshift(getRandomItemFromArray(availablePipes));
-
-    if (!nextPipe) {
-      throw new Error("no next pipe available");
-    }
-
     if (tile instanceof HTMLElement) {
+      let nextPipe = upcomingPipes.pop();
+      upcomingPipes.unshift(getRandomItemFromArray(AVAILABLE_PIPES));
+      if (!nextPipe) {
+        throw new Error("no next pipe available");
+      }
       const row = tile.dataset.row;
       const col = tile.dataset.col;
-
       grid.set(`${row},${col}`, nextPipe);
-
-      gridEl.innerHTML = renderGrid();
-
-      upcomingEl.innerHTML = renderUpcomingPipes();
+      gridEl.innerHTML = renderGrid(state);
+      upcomingEl.innerHTML = renderUpcomingPipes(state);
     }
   }
 });
